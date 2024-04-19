e2a5902d22e3ff274ebeac3bd0a22a706d455b8c
Sudipta Basak <sudiptab2100@gmail.com>
2024-04-20 00:52:51 +0530

    Encryption protocol added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { createLibp2p } from 'libp2p'
 import { tcp } from '@libp2p/tcp'
+import { noise } from '@chainsafe/libp2p-noise'
 
 const node = await createLibp2p({
-  transports: [tcp()]
+  transports: [tcp()],
+  connectionEncryption: [noise()]
 })
